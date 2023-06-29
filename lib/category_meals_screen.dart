6f1a4b0dfa07fa02ccc27a3f658164330843aaa6
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-06-30 01:43:12 +0530

    printing meals detai

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './dummy_data.dart';
 class CategoryMealsScreen extends StatelessWidget {
     // final String categoryId;
     // final String categoryTitle;
@@ -13,14 +13,18 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs =ModalRoute.of(context).settings.arguments as Map<String,String>;
     final categoryId=routeArgs['id'];
     final categoryTitle=routeArgs['title'];
+    // extracting the categories with given id from meals
+    final categoryMeals=DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
     return
     Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body:  Center(
-      child: Text("The Recipies For The Category"),
-    ),
+      body: ListView.builder(itemBuilder: (ctx,index){
+   return Text(categoryMeals[index].title);
+      },itemCount: categoryMeals.length,)
     )
     ;
   }
